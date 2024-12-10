120234ffa9858aa00ad20a0a20e819afe9d2e2ac
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2024-12-10 14:03:27 +0300

    initial-1

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
     res.send({ message: "Hello Service" });
 });
 
+app.get("/user", (req, res) => {
+  res.send({ 
+    developer: "Abdulrahman Haaji",
+    company: "Tabarak ICT",
+    depart: "Software Development",
+    ID: "002"
+
+   });
+});
+
+app.get("/server", (req, res) => {
+  res.send({
+  server: "node-js service",
+  port: 2400
+  });
+});
+
+
+
 const PORT = 2400; // App listens on port 3200
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
